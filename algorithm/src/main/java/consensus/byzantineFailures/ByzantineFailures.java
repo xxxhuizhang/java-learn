37610fc0fe20f318876aecc7217693dd6f81b27e
xxxhuizhang <xxxhuizhang@qq.com>
2021-12-28 11:05:40 +0800

    修改

---
+++ algorithm/src/main/java/consensus/byzantineFailures/ByzantineFailures.java
@@ -0,0 +1,11 @@
+package consensus.byzantineFailures;
+
+/**
+ * 拜占庭将军问题（Byzantine failures），
+ * 是由莱斯利·兰伯特提出的点对点通信中的基本问题。
+ * 含义是在存在消息丢失的不可靠信道上试图通过消息传递的方式达到一致性是不可能的
+ *
+ *
+ */
+public class ByzantineFailures {
+}
---
+++ algorithm/src/main/java/consensus/raft/Raft.java
@@ -0,0 +1,14 @@
+package consensus.raft;
+
+/*
+ *   Raft Consensus Algorithm  共识算法
+ * 
+ * CAP
+ * BASE
+ * Quorum
+ * Consist Hashing
+ */
+
+public class Raft {
+
+}
