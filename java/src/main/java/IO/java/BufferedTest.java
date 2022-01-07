d0f21ef77e16d6f91ab0bd241d5b5210debebc49
xxxhuizhang <xxxhuizhang@qq.com>
2022-01-07 13:38:02 +0800

    修改

--- java/src/main/java/IO/java/BufferedTest.java
+++ java/src/main/java/IO/java/BufferedTest.java
@@ -5,21 +5,20 @@ import org.junit.Test;
 import java.io.*;
 
 /**
- *
- *  https://blog.csdn.net/zhaoyanjun6/article/details/54292148
- *
- *
+ * https://blog.csdn.net/zhaoyanjun6/article/details/54292148
+ * <p>
+ * <p>
  * 处理流之一：缓冲流的使用
- *
+ * <p>
  * 1.缓冲流：
  * BufferedInputStream
  * BufferedOutputStream
  * BufferedReader
  * BufferedWriter
- *
+ * <p>
  * 2.作用：提供流的读取、写入的速度
- *   提高读写速度的原因：内部提供了一个缓冲区
- *
+ * 提高读写速度的原因：内部提供了一个缓冲区
+ * <p>
  * 3. 处理流，就是“套接”在已有的流的基础上。
  *
  * @author shkstart
@@ -27,129 +26,44 @@ import java.io.*;
  */
 public class BufferedTest {
 
-    /*
-    实现非文本文件的复制
-     */
     @Test
-    public void BufferedStreamTest() throws FileNotFoundException {
+    public void BufferedStreamTest() {
+        String srcPath = "./file/io/爱情与友情.jpg";
+        String destPath = "./file/io/爱情与友情2.jpg";
+        BufferedStreamCopy(srcPath, destPath);
+    }
+
+    public void BufferedStreamCopy(String srcPath, String destPath) {
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
-
         try {
-            //1.造文件
-            File srcFile = new File("爱情与友情.jpg");
-            File destFile = new File("爱情与友情3.jpg");
-            //2.造流
-            //2.1 造节点流
-            FileInputStream fis = new FileInputStream((srcFile));
-            FileOutputStream fos = new FileOutputStream(destFile);
-            //2.2 造缓冲流
-            bis = new BufferedInputStream(fis);
-            bos = new BufferedOutputStream(fos);
-
-            //3.复制的细节：读取、写入
+            bis = new BufferedInputStream(new FileInputStream((new File(srcPath))));
+            bos = new BufferedOutputStream(new FileOutputStream(new File(destPath)));
+            int len;
             byte[] buffer = new byte[10];
-            int len;
-            while((len = bis.read(buffer)) != -1){
-                bos.write(buffer,0,len);
-
+            while ((len = bis.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
 //                bos.flush();//刷新缓冲区
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            //4.资源关闭
-            //要求：先关闭外层的流，再关闭内层的流
-            if(bos != null){
+            //说明：关闭外层流的同时，内层流也会自动的进行关闭.
+            if (bos != null) {
                 try {
                     bos.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
-            if(bis != null){
+            if (bis != null) {
                 try {
                     bis.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
-            //说明：关闭外层流的同时，内层流也会自动的进行关闭。关于内层流的关闭，我们可以省略.
-//        fos.close();
-//        fis.close();
         }
-
-
-
-    }
-
-    //实现文件复制的方法
-    public void copyFileWithBuffered(String srcPath,String destPath){
-        BufferedInputStream bis = null;
-        BufferedOutputStream bos = null;
-
-        try {
-            //1.造文件
-            File srcFile = new File(srcPath);
-            File destFile = new File(destPath);
-            //2.造流
-            //2.1 造节点流
-            FileInputStream fis = new FileInputStream((srcFile));
-            FileOutputStream fos = new FileOutputStream(destFile);
-            //2.2 造缓冲流
-            bis = new BufferedInputStream(fis);
-            bos = new BufferedOutputStream(fos);
-
-            //3.复制的细节：读取、写入
-            byte[] buffer = new byte[1024];
-            int len;
-            while((len = bis.read(buffer)) != -1){
-                bos.write(buffer,0,len);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            //4.资源关闭
-            //要求：先关闭外层的流，再关闭内层的流
-            if(bos != null){
-                try {
-                    bos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-            if(bis != null){
-                try {
-                    bis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-            //说明：关闭外层流的同时，内层流也会自动的进行关闭。关于内层流的关闭，我们可以省略.
-//        fos.close();
-//        fis.close();
-        }
-    }
-
-    @Test
-    public void testCopyFileWithBuffered(){
-        long start = System.currentTimeMillis();
-
-        String srcPath = "C:\\Users\\Administrator\\Desktop\\01-视频.avi";
-        String destPath = "C:\\Users\\Administrator\\Desktop\\03-视频.avi";
-
-
-        copyFileWithBuffered(srcPath,destPath);
-
-
-        long end = System.currentTimeMillis();
-
-        System.out.println("复制操作花费的时间为：" + (end - start));//618 - 176
     }
 
 
@@ -158,54 +72,47 @@ public class BufferedTest {
 
      */
     @Test
-    public void testBufferedReaderBufferedWriter(){
+    public void testBufferedReaderBufferedWriter() {
         BufferedReader br = null;
         BufferedWriter bw = null;
         try {
             //创建文件和相应的流
             br = new BufferedReader(new FileReader(new File("dbcp.txt")));
             bw = new BufferedWriter(new FileWriter(new File("dbcp1.txt")));
-
             //读写操作
             //方式一：使用char[]数组
 //            char[] cbuf = new char[1024];
 //            int len;
 //            while((len = br.read(cbuf)) != -1){
 //                bw.write(cbuf,0,len);
-//    //            bw.flush();
+//                bw.flush();
 //            }
-
             //方式二：使用String
             String data;
-            while((data = br.readLine()) != null){
+            while ((data = br.readLine()) != null) {
                 //方法一：
 //                bw.write(data + "\n");//data中不包含换行符
                 //方法二：
                 bw.write(data);//data中不包含换行符
                 bw.newLine();//提供换行的操作
-
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             //关闭资源
-            if(bw != null){
-
+            if (bw != null) {
                 try {
                     bw.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if(br != null){
+            if (br != null) {
                 try {
                     br.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
         }
 
