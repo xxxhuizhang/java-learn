99770930c4143f6f826926c62fd1dc59a9ccf280
xxxhuizhang <xxxhuizhang@qq.com>
2022-06-02 15:41:55 +0800

    postgresql

--- learn/postgres/learn.sql
+++ learn/postgres/learn.sql
@@ -37,6 +37,11 @@ select coalesce('1','2'); --postgresql
 --select ifnull('1','2'); --mysql
 
 
+select position('abd' in 'abcdab');
+select strpos( 'abcdab','cd');
+select  'ABcdef' ~* 'a.*f';  --  ~ 匹配   ~* 匹配(case ignroe)  !~ 不匹配
+
+
 create unique index grouId_and_memberId on chat_member (group_id, member_id);
 alter table if exists chat_message add constraint FKdgx6rii6jdb7vcelx67r1h2r1 foreign key (group_id) references chat_group;
 alter table if exists chat_member add constraint grouId_and_memberId unique (group_id, member_id);
